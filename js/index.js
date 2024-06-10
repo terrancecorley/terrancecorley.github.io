4b968ca0c9635aab2320eee2325811a0f47f1869
Terrance Corley <terrancecorley@outlook.com>
2024-06-09 20:32:54 -0700

    tas-39 | disabled spotlight effect

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 const main = (() => {
     window.onload = () => {
-        const commonLaptopDisplayWidth = 1366;
-        if (window.innerWidth >= commonLaptopDisplayWidth) {
-            const docBody = document.querySelector('body');
+        // const commonLaptopDisplayWidth = 1366;
+
+        // // todo: fix spotlight bug on larger displays, has to do with body height/position
+        // if (window.innerWidth >= commonLaptopDisplayWidth) {
+        //     const docBody = document.querySelector('body');
     
-            docBody.addEventListener('mousemove', (e) => {
-                let spotlightSize = '#fdfdfd 120px, rgba(248, 247, 244, 1) 200px)';
+        //     docBody.addEventListener('mousemove', (e) => {
+        //         let spotlightSize = '#fdfdfd 120px, rgba(248, 247, 244, 1) 200px)';
     
-                docBody.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 80}%, ${spotlightSize}`;
-            })
-        }
+        //         docBody.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 80}%, ${spotlightSize}`;
+        //     })
+        // }
     };
 })();
